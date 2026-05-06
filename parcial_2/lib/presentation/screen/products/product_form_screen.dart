594c12471be55c7fd421410dce0c154a16fa5b88
mmi-cmd <mjperezgo@ufpso.edu.co>
2026-05-06 09:10:33 -0500

    cambios
    Removed duplicate comment about pagination in product form screen.

--- parcial_2/lib/presentation/screen/products/product_form_screen.dart
+++ parcial_2/lib/presentation/screen/products/product_form_screen.dart
@@ -60,7 +60,7 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
         ),
       );
       
-      context.go('/products'); //los nuevos productos se obtienen con la paginación usando el paginación usando el ScrollController
+      context.go('/products'); //los nuevos productos se obtienen con la paginación usando el ScrollController
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text(error), backgroundColor: Colors.red),
